66593d1f8d49bf0e80fd2ca2b66e8431b84070a1
Afonso Bordado <afonsobordado@az8.co>
2017-04-28 10:06:05 +0000

    Add build_op macro

--- src/builder.rs
+++ src/builder.rs
@@ -4,46 +4,63 @@ use llvm_sys::core as llvm;
 
 use super::*;
 
+macro_rules! build_op_str {
+    ($op_name: ident, $fn: path, $($argn: ident: $argv: path),*) => {
+        impl Builder {
+            pub fn $op_name(&mut self, $($argn: $argv),*, name: &str) -> LLVMValueRef {
+                let c_name = CString::new(name).unwrap();
+                unsafe {
+                    $fn(self.ptr, $($argn),*, c_name.as_ptr())
+                }
+            }
+        }
+    }
+}
+
+macro_rules! build_op {
+    ($op_name: ident, $fn: path, $($argn: ident: $argv: path),*) => {
+        impl Builder {
+            pub fn $op_name(&mut self, $($argn: $argv),*) -> LLVMValueRef {
+                unsafe {
+                    $fn(self.ptr, $($argn),*)
+                }
+            }
+        }
+    }
+}
+
 pub struct Builder {
     pub ptr: LLVMBuilderRef
 }
+
+build_op!(build_ret, llvm::LLVMBuildRet, ret_val: LLVMValueRef);
+build_op!(build_ret_void, llvm::LLVMBuildRetVoid,); // TODO: Fix the trailing comma
+build_op!(build_store, llvm::LLVMBuildStore, val: LLVMValueRef, ptr: LLVMValueRef);
+build_op!(build_br, llvm::LLVMBuildBr, dest: LLVMBasicBlockRef);
+
+build_op!(build_cond_br, llvm::LLVMBuildCondBr, cond: LLVMValueRef,
+                                                then: LLVMBasicBlockRef,
+                                                else_: LLVMBasicBlockRef);
+
+build_op_str!(build_load, llvm::LLVMBuildLoad, ptr: LLVMValueRef);
+build_op_str!(build_alloca, llvm::LLVMBuildAlloca, ty: LLVMTypeRef);
+
+build_op_str!(build_add, llvm::LLVMBuildAdd, lhs: LLVMValueRef, rhs: LLVMValueRef);
+build_op_str!(build_sub, llvm::LLVMBuildSub, lhs: LLVMValueRef, rhs: LLVMValueRef);
+build_op_str!(build_mul, llvm::LLVMBuildMul, lhs: LLVMValueRef, rhs: LLVMValueRef);
+build_op_str!(build_sdiv, llvm::LLVMBuildSDiv, lhs: LLVMValueRef, rhs: LLVMValueRef);
+
+///Comment
+build_op_str!(build_icmp, llvm::LLVMBuildICmp, op: LLVMIntPredicate,
+                                               lhs: LLVMValueRef,
+                                               rhs: LLVMValueRef);
+
+
 
 impl Builder {
     pub fn position_at_end(&mut self, basic_block: LLVMBasicBlockRef) {
         unsafe {
             llvm::LLVMPositionBuilderAtEnd(self.ptr, basic_block);
-        }
-    }
-
-    pub fn build_ret(&mut self, ret_val: LLVMValueRef) -> LLVMValueRef {
-        unsafe {
-            llvm::LLVMBuildRet(self.ptr, ret_val)
-        }
-    }
-
-    pub fn build_ret_void(&self) -> LLVMValueRef {
-        unsafe {
-            llvm::LLVMBuildRetVoid(self.ptr)
-        }
-    }
-
-    pub fn build_alloca(&mut self, ty: LLVMTypeRef, name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildAlloca(self.ptr, ty, c_name.as_ptr())
-        }
-    }
-
-    pub fn build_store(&mut self, val: LLVMValueRef, ptr: LLVMValueRef) -> LLVMValueRef {
-        unsafe {
-            llvm::LLVMBuildStore(self.ptr, val, ptr)
-        }
-    }
-
-    pub fn build_load(&mut self, ptr: LLVMValueRef, name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildLoad(self.ptr, ptr, c_name.as_ptr())
         }
     }
 
@@ -60,44 +77,6 @@ impl Builder {
             )
         }
     }
-
-    pub fn build_add(&mut self, lhs: LLVMValueRef, rhs: LLVMValueRef, name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildAdd(self.ptr, lhs, rhs, c_name.as_ptr())
-        }
-    }
-
-    pub fn build_sub(&mut self, lhs: LLVMValueRef, rhs: LLVMValueRef, name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildSub(self.ptr, lhs, rhs, c_name.as_ptr())
-        }
-    }
-
-    pub fn build_mul(&mut self, lhs: LLVMValueRef, rhs: LLVMValueRef, name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildMul(self.ptr, lhs, rhs, c_name.as_ptr())
-        }
-    }
-
-    pub fn build_sdiv(&mut self, lhs: LLVMValueRef, rhs: LLVMValueRef,
-                      name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildSDiv(self.ptr, lhs, rhs, c_name.as_ptr())
-        }
-    }
-
-    pub fn build_icmp(&mut self, op: LLVMIntPredicate, lhs: LLVMValueRef, rhs: LLVMValueRef,
-                      name: &str) -> LLVMValueRef {
-        let c_name = CString::new(name).unwrap();
-        unsafe {
-            llvm::LLVMBuildICmp(self.ptr, op, lhs, rhs, c_name.as_ptr())
-        }
-    }
-
     pub fn build_global_string(&self, s: &str, name: &str) -> LLVMValueRef {
         let c_s = CString::new(s).unwrap();
         let c_name = CString::new(name).unwrap();
@@ -114,20 +93,6 @@ impl Builder {
                                        indices.len() as u32, c_name.as_ptr())
         }
     }
-
-    pub fn build_cond_br(&self, cond: LLVMValueRef, then: LLVMBasicBlockRef,
-                         else_: LLVMBasicBlockRef) -> LLVMValueRef {
-        unsafe {
-            llvm::LLVMBuildCondBr(self.ptr, cond, then, else_)
-        }
-    }
-
-    pub fn build_br(&self, dest: LLVMBasicBlockRef) -> LLVMValueRef {
-        unsafe {
-            llvm::LLVMBuildBr(self.ptr, dest)
-        }
-    }
-
 }
 
 impl Drop for Builder {
